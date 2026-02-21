444d9e2de59fa8cc9408dff6cb2b12dd9192b404
beatrice168 <helgapaul389@gmail.com>
2026-02-22 00:37:33 +0300

    added flask

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -2,17 +2,16 @@ from sql_connection import  get_sql_connection
 
 def get_all_products(connection):
     
-
-    cursor = connection.cursor() 
+    cursor_obj = connection.cursor() 
 
     query=("SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom "
        "FROM products INNER JOIN uom on products.uom_id=uom.uom_id")
-    cursor.execute(query)
+    cursor_obj.execute(query) #running the query 
 
-    response = []
+    response = [] #empty shopping list 
 
-
-    for(product_id, name, uom_id, price_per_unit, uom) in cursor:
+     #for each product found add it to our list
+    for(product_id, name, uom_id, price_per_unit, uom) in cursor_obj:
      response.append({
         'product_id': product_id,
         'name': name, 
@@ -22,29 +21,31 @@ def get_all_products(connection):
      })
 
 
-    return response 
+    return response  #return the list
 
 def insert_new_product(connection, product):
-   cursor=connection.cursor()
+   cursor_obj=connection.cursor()
    query=("INSERT INTO products "
           " (name, uom_id, price_per_unit)"
           " VALUES (%s, %s, %s)")
    
    data=(product['product_name'], product['uom_id'], product['price_per_unit'])
 
-   cursor.execute(query, data)
-   connection.commit()
-   return cursor.lastrowid
+   cursor_obj.execute(query, data) #add the new product
+   connection.commit() # save te changes permanently 
+   return cursor_obj.lastrowid # returning the new product's id 
 
 def delete_product(connection, product_id):
-   cursor = connection.cursor()
+   cursor_obj = connection.cursor()
    query = ("DELETE FROM products where product_id=" + str(product_id))
-   cursor.execute(query)
+   cursor_obj.execute(query)
    connection.commit()
 
 
 
 
 if __name__=='__main__':
-    connection = get_sql_connection()
-    print(delete_product(connection, 3))
+    connection = get_sql_connection() #first connect to db 
+    print(get_all_products(connection))
+
+    # print(delete_product(connection, 3)) #the delete the product with id 3 
--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
-__cnx=None
+__cnx=None #stores connection (starts empty)
 
 
 def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-
+    global __cnx #use the connection we saved before
+    if __cnx is None: #if no connection exists yet
+        #create a connection to the database 
         __cnx = mysql.connector.connect(user='root', password='12345678',
                               host='127.0.0.1',
                               database='grocery_store')
-    return __cnx
+    return __cnx #give back the connection
